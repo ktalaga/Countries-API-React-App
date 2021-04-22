b4bc707bb1533e49f0e02dd22f013725f642f980
konrad <ktalaga90@gmail.com>
2021-04-22 19:42:35 +0100

    Added side effect to populate state of the container with fetch data with no interaction from user

--- src/container/CountryContainer.js
+++ src/container/CountryContainer.js
@@ -1,14 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import CountrySelector from '../component/CountrySelector';
 
 const CountryContainer = () => {
 
     const [countries, setCountries] = useState([])
 
+    useEffect(() => {
+        fetchCountries()
+    })
+
     const fetchCountries = () => {
         fetch("https://restcountries.eu/rest/v2/all")
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(countries => setCountries(countries))
     }
 
     return(
