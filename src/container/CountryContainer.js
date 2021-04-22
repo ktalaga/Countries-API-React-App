b10cf3c19941aa9f8dea73e64a0d9662f627426b
konrad <ktalaga90@gmail.com>
2021-04-22 19:31:35 +0100

    imported useState, set state for the container, wrote fetch function to fetch data from api, all works so far

--- src/container/CountryContainer.js
+++ src/container/CountryContainer.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CountrySelector from '../component/CountrySelector';
 
 const CountryContainer = () => {
 
+    const [countries, setCountries] = useState([])
 
+    const fetchCountries = () => {
+        fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
 
     return(
         <div>
